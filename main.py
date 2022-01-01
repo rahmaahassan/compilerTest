39453d326ff8d4c8622c5d5a637a2352ed5f8d07
Rahma Hassan <rahmahassan536@gmail.com>
2022-01-01 09:17:51 +0200

    add lists

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import AtL
 while True:
     text = input('@L>')
     # print(text)
-    result,error = AtL.run('<stdin>',text)
-    if error: print(error.as_string())
+    result, error = AtL.run('<stdin>', text)
+    if error:
+        print(error.as_string())
     elif result:
         print(result)
